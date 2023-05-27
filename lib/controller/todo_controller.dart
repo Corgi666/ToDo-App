4c2cc4ea35ac405a85441a62e489c5a3f374ff90
Corgi666 <anuchit.k1996@gmail.com>
2023-05-27 19:40:42 +0700

    update

--- lib/controller/todo_controller.dart
+++ lib/controller/todo_controller.dart
@@ -4,38 +4,35 @@ import '../model/ToDo.dart';
 
 class ToDoController extends GetxController {
   final todoList = [].obs;
+  List<ToDo> totoList = [
+    ToDo(id: '01', todoText: 'Morning Exercise', isDone: true),
+    ToDo(id: '02', todoText: 'Buy Groceries', isDone: true),
+    ToDo(id: '03', todoText: 'Check Emails'),
+    ToDo(id: '04', todoText: 'Team Meeting'),
+    ToDo(id: '05', todoText: 'Work on mobile apps for 2 hours'),
+    ToDo(id: '06', todoText: 'Dinner with Jenny'),
+  ];
   @override
   void onReady() {
     // TODO: implement onReady
-    totoList();
-    todoList.value = totoList();
+    totoList;
+    todoList.value = totoList;
     super.onReady();
-  }
-
-  static List<ToDo> totoList() {
-    return [
-      ToDo(id: '01', todoText: 'Morning Exercise', isDone: true),
-      ToDo(id: '02', todoText: 'Buy Groceries', isDone: true),
-      ToDo(id: '03', todoText: 'Check Emails'),
-      ToDo(id: '04', todoText: 'Team Meeting'),
-      ToDo(id: '05', todoText: 'Work on mobile apps for 2 hours'),
-      ToDo(id: '06', todoText: 'Dinner with Jenny'),
-    ];
   }
 
   void addData(
       {required String id, required String todoText, bool isDone = false}) {
     final newToDo = ToDo(id: id, todoText: todoText, isDone: isDone);
 
-    totoList().add(newToDo);
+    totoList.add(newToDo);
     print(newToDo);
     print(todoList.length);
-    print(totoList().length);
+    print(totoList.length);
     print('add Data');
-    todoList.value = totoList();
+    todoList.value = totoList;
   }
 
   void deleteData(String id) {
-    totoList().removeWhere((todo) => todo.id == id);
+    totoList.removeWhere((todo) => todo.id == id);
   }
 }
